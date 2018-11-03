10edb66398d9fa88a3b1178a51484fcdd9255552
jerryqypan <qp7@duke.edu>
2018-11-03 12:02:00 -0400

    started corner database impl

--- rubik.py
+++ rubik.py
@@ -1,14 +1,82 @@
 import numpy as np
 
 
-class Rubik:
-    def __init__(self, size=3):
-        n_face = (size*size-1) if size%2 else (size*size)
-        n_size = n_face*6
-        face_map = {"U":4,"L":0,"F":1,"R":2,"B":3,"D":5}
-        self.state = np.zeros((6,size,size))
+class Cube:
+    def __init__(self):
+        edges = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        corners = [0, 1, 2, 3, 4, 5, 6, 7]
 
-    def __fill_face(self):
-        ...
+    def twist(self, move):
+        if move == "U":
+            ...
 
+corners = {
+    "UFR": 0,
+    "URB": 1,
+    "UBL": 2,
+    "ULF": 3,
+    "DRF": 4,
+    "DFL": 5,
+    "DLB": 6,
+    "DBR": 7,
+    "FRU": 8,
+    "RBU": 9,
+    "BLU": 10,
+    "LFU": 11,
+    "RFD": 12,
+    "FLD": 13,
+    "LBD": 14,
+    "BRD": 15,
+    "RUF": 16,
+    "BUR": 17,
+    "LUB": 18,
+    "FUL": 19,
+    "FDR": 20,
+    "LDF": 21,
+    "BDL": 22,
+    "RDB": 23
+}
 
+moves = {
+    "U": [0,1,2,3],
+    "F": [0,3,4,7],
+    "R": [0,1,4,5],
+    "D": [4,5,6,7],
+    "B": [1,2,5,6],
+    "L": [2,3,6,7]
+}
+
+moves_orientation = {
+    "U": [0,0,0,0],
+    "F": [1,2,1,2],
+    "F'": [2,1,2,1],
+    "R": [1,2,]
+
+}
+
+edges = {
+    "UF": 0,
+    "UR": 1,
+    "UB": 2,
+    "UL": 3,
+    "DF": 4,
+    "DR": 5,
+    "DB": 6,
+    "DL": 7,
+    "FR": 8,
+    "FL": 9,
+    "BR": 10,
+    "BL": 11,
+    "FU": 12,
+    "RU": 13,
+    "BU": 14,
+    "LU": 15,
+    "FD": 16,
+    "RD": 17,
+    "BD": 18,
+    "LD": 19,
+    "RF": 20,
+    "LF": 21,
+    "RB": 22,
+    "LB": 23
+}
